b0a9f81326983a25cdb2ff3158411577efe957d5
isaikin.victor <isaikin@bezlimit.ru>
2023-03-17 13:28:53 +0600

    init

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-/// <reference lib="webworker" />
-
-export { default as Mazu } from './Mazu';
-export { default as EventListener } from './EventListener';
-export { default as Listener } from './Listener';
-export { default as Strategy } from './Strategy';
